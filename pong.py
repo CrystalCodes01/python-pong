3d0091b88320c671049e706103533bb7e704b6a2
CrystalMaria <crystalcodes.0@gmail.com>
2017-08-09 14:26:20 -0400

    gui

--- pong.py
+++ pong.py
@@ -1,9 +1,9 @@
 # classic arcade game Pong
-# try:
+try:
     import simplegui
 except ImportError:
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
-import random
+
 
 import os
 # from flask import Flask
